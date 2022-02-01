1b0f56ccee77a1ab5fe24f26795276fff8aed6ce
rogerrqueso5 <96122500+rogerrqueso5@users.noreply.github.com>
2022-02-01 13:56:47 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let y = 0
+let x = 0
+let pintar_x = 0
 basic.forever(function () {
-	
+    y = input.acceleration(Dimension.Y)
+    x = input.acceleration(Dimension.X)
+    led.unplot(pintar_x, 0)
+    if (x < -600) {
+        pintar_x = 0
+    } else if (x < -300) {
+        pintar_x = 1
+    } else if (x > 600) {
+        pintar_x = 4
+    } else if (x > 300) {
+        pintar_x = 3
+    } else {
+        pintar_x = 2
+    }
+    led.plot(pintar_x, 0)
 })
